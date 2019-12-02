e77ad47798d7b615b10fc7ab1dbb27db683c9557
Emil Lundberg <emil@emlun.se>
2019-12-02 06:23:12 +0100

    Solve day 2b

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -13,21 +13,52 @@ fn step(iep: usize, mut prog: Vec<usize>) -> (usize, Vec<usize>) {
     (iep + 4, prog)
 }
 
-pub fn solve(lines: &[String]) -> Solution {
-    let mut program: Vec<usize> = lines[0].split(',').map(|s| s.parse().unwrap()).collect();
+fn run(mut program: Vec<usize>) -> usize {
     let mut iep = 0;
-
-    println!("{} : {:?}", iep, program);
-
-    program[1] = 12;
-    program[2] = 2;
+    // println!("{} : {:?}", iep, program);
 
     while program[iep] != 99 {
         let out = step(iep, program);
         iep = out.0;
         program = out.1;
-        println!("{} : {:?}", iep, program);
+        // println!("{} : {:?}", iep, program);
     }
 
-    (program[0].to_string(), "bar".to_string())
+    program[0]
+}
+
+fn solve_a(mut program: Vec<usize>) -> usize {
+    let mut iep = 0;
+    println!("{} : {:?}", iep, program);
+
+    program[1] = 12;
+    program[2] = 2;
+
+    run(program)
+}
+
+fn solve_b(program: Vec<usize>) -> usize {
+    for noun in 0..program.len() {
+        println!("noun: {}", noun);
+        for verb in 0..program.len() {
+            let mut prog = program.clone();
+            prog[1] = noun;
+            prog[2] = verb;
+            let output = run(prog);
+            if output == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+
+    panic!("No solution found!");
+}
+
+pub fn solve(lines: &[String]) -> Solution {
+    let program: Vec<usize> = lines[0].split(',').map(|s| s.parse().unwrap()).collect();
+
+    let a_solution = solve_a(program.clone());
+    let b_solution = solve_b(program.clone());
+
+    (a_solution.to_string(), b_solution.to_string())
 }
