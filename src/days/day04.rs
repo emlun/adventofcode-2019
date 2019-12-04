88810faf6673c19a635ca12f1cf7b16e6c0c7969
Emil Lundberg <emil@emlun.se>
2019-12-04 07:22:16 +0100

    Clean up day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,30 +1,26 @@
 use crate::common::Solution;
 use std::collections::HashMap;
 
-fn satisfies_conditions(password: u32) -> bool {
+fn satisfies_conditions_a(password: u32) -> bool {
     let s = password.to_string();
-    let mut two_same: bool = false;
+
+    for i in 1..s.len() {
+        let i1 = s.chars().nth(i - 1).unwrap() as u8 - 0x30;
+        let i2 = s.chars().nth(i).unwrap() as u8 - 0x30;
+        if i1 > i2 {
+            return false;
+        }
+    }
+
     for i in 1..s.len() {
         if s.chars().nth(i - 1) == s.chars().nth(i) {
-            two_same = true;
-            break;
+            return true;
         }
     }
-
-    let mut increasing: bool = true;
-    for i in 1..s.len() {
-        let i1 = (s.chars().nth(i - 1).unwrap() as u8 - 0x30);
-        let i2 = (s.chars().nth(i).unwrap() as u8 - 0x30);
-        if i1 > i2 {
-            increasing = false;
-            break;
-        }
-    }
-
-    return two_same && increasing;
+    return false;
 }
 
-fn satisfies_conditions_b(password: u32) -> bool {
+fn satisfies_new_conditions_b(password: u32) -> bool {
     let s = password.to_string();
     let mut sames: HashMap<char, u8> = HashMap::new();
     for i in 1..s.len() {
@@ -33,19 +29,7 @@ fn satisfies_conditions_b(password: u32) -> bool {
             sames.insert(c1, sames.get(&c1).unwrap_or(&0) + 1);
         }
     }
-    let two_same: bool = sames.values().any(|i| i == &1);
-
-    let mut increasing: bool = true;
-    for i in 1..s.len() {
-        let i1 = (s.chars().nth(i - 1).unwrap() as u8 - 0x30);
-        let i2 = (s.chars().nth(i).unwrap() as u8 - 0x30);
-        if i1 > i2 {
-            increasing = false;
-            break;
-        }
-    }
-
-    return two_same && increasing;
+    return sames.values().any(|i| i == &1);
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -56,19 +40,16 @@ pub fn solve(lines: &[String]) -> Solution {
     let low_bound: u32 = bounds[0];
     let high_bound: u32 = bounds[1];
 
-    let mut count = 0;
+    let mut count_a = 0;
     let mut count_b = 0;
     for password in low_bound..=high_bound {
-        if satisfies_conditions(password) {
-            count += 1;
-        }
-
-        if satisfies_conditions_b(password) {
-            count_b += 1;
+        if satisfies_conditions_a(password) {
+            count_a += 1;
+            if satisfies_new_conditions_b(password) {
+                count_b += 1;
+            }
         }
     }
 
-    let a_solution = count;
-    let b_solution = count_b;
-    (a_solution.to_string(), b_solution.to_string())
+    (count_a.to_string(), count_b.to_string())
 }
