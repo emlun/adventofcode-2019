21af5cf92668a6b811c3fd7eb07e22601264f5ac
Emil Lundberg <emil@emlun.se>
2019-12-04 06:43:46 +0100

    Solve day 4b

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,4 +1,5 @@
 use crate::common::Solution;
+use std::collections::HashMap;
 
 fn satisfies_conditions(password: u32) -> bool {
     let s = password.to_string();
@@ -20,7 +21,29 @@ fn satisfies_conditions(password: u32) -> bool {
         }
     }
 
-    // println!("{} {} {}", password, two_same, increasing);
+    return two_same && increasing;
+}
+
+fn satisfies_conditions_b(password: u32) -> bool {
+    let s = password.to_string();
+    let mut sames: HashMap<char, u8> = HashMap::new();
+    for i in 1..s.len() {
+        let c1 = s.chars().nth(i - 1).unwrap();
+        if c1 == s.chars().nth(i).unwrap() {
+            sames.insert(c1, sames.get(&c1).unwrap_or(&0) + 1);
+        }
+    }
+    let two_same: bool = sames.values().any(|i| i == &1);
+
+    let mut increasing: bool = true;
+    for i in 1..s.len() {
+        let i1 = (s.chars().nth(i - 1).unwrap() as u8 - 0x30);
+        let i2 = (s.chars().nth(i).unwrap() as u8 - 0x30);
+        if i1 > i2 {
+            increasing = false;
+            break;
+        }
+    }
 
     return two_same && increasing;
 }
@@ -34,17 +57,18 @@ pub fn solve(lines: &[String]) -> Solution {
     let high_bound: u32 = bounds[1];
 
     let mut count = 0;
+    let mut count_b = 0;
     for password in low_bound..=high_bound {
         if satisfies_conditions(password) {
             count += 1;
         }
+
+        if satisfies_conditions_b(password) {
+            count_b += 1;
+        }
     }
 
-    println!("{}", satisfies_conditions(111111));
-    println!("{}", satisfies_conditions(223450));
-    println!("{}", satisfies_conditions(123789));
-
     let a_solution = count;
-    let b_solution = "bar";
+    let b_solution = count_b;
     (a_solution.to_string(), b_solution.to_string())
 }
