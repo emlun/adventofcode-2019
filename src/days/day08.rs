ffacdb7f4e4f89ee28b22d0f79bf87a7c1738f6e
Emil Lundberg <emil@emlun.se>
2019-12-08 14:48:08 +0100

    Solve day 8b

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,10 +1,50 @@
 use crate::common::Solution;
 
+const W: usize = 25;
+const H: usize = 6;
+
+pub fn solve_b(layers: &Vec<Vec<u8>>) -> String {
+    let mut image: Vec<Vec<u8>> = Vec::new();
+    for _ in 0..H {
+        let mut row: Vec<u8> = Vec::new();
+        for _ in 0..W {
+            row.push(2);
+        }
+        image.push(row);
+    }
+    for layer in layers.iter() {
+        for r in 0..H {
+            for c in 0..W {
+                if image[r][c] == 2 {
+                    image[r][c] = layer[r * W + c];
+                }
+            }
+        }
+    }
+
+    format!(
+        "\n{}",
+        image
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|cell| match cell {
+                        0 => ".",
+                        1 => "#",
+                        2 => " ",
+                        _ => unreachable!(),
+                    })
+                    .collect::<Vec<&str>>()
+                    .join("")
+            })
+            .collect::<Vec<String>>()
+            .join("\n")
+    )
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let mut digits = lines.iter().flat_map(|line| line.chars());
     let mut images: Vec<Vec<u8>> = Vec::new();
-    let W = 25;
-    let H = 6;
     while let Some(digit0) = digits.next() {
         let mut img = Vec::new();
         img.push(digit0.to_string().parse().unwrap());
@@ -27,6 +67,7 @@ pub fn solve(lines: &[String]) -> Solution {
     let num_ones = fewest_zeros.iter().filter(|d| **d == 1).count();
     let num_twos = fewest_zeros.iter().filter(|d| **d == 2).count();
     let a_solution = num_ones * num_twos;
+    let b_solution = solve_b(&images);
 
-    (a_solution.to_string(), "bar".to_string())
+    (a_solution.to_string(), b_solution.to_string())
 }
