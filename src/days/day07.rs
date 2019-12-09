a16ddbe3e45075f96cc11eb3e17aebd47dc9929d
Emil Lundberg <emil@emlun.se>
2019-12-09 02:14:17 +0100

    Use permutation iterator in day 7

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,4 +1,5 @@
 use crate::common::Solution;
+use crate::util::Permutations;
 
 fn step(
     eip: usize,
@@ -116,30 +117,19 @@ fn run(mut program: Vec<i32>, input: (u8, i32)) -> Vec<i32> {
 fn solve_a(program: Vec<i32>) -> i32 {
     let mut max_output = None;
 
-    for i0 in 0..=4 {
-        for i1 in 0..=4 {
-            if i0 != i1 {
-                for i2 in 0..=4 {
-                    if i2 != i0 && i2 != i1 {
-                        for i3 in 0..=4 {
-                            if i3 != i0 && i3 != i1 && i3 != i2 {
-                                for i4 in 0..=4 {
-                                    if i4 != i0 && i4 != i1 && i4 != i2 && i4 != i3 {
-                                        let output0 = run(program.clone(), (i0, 0));
-                                        let output1 = run(program.clone(), (i1, output0[0]));
-                                        let output2 = run(program.clone(), (i2, output1[0]));
-                                        let output3 = run(program.clone(), (i3, output2[0]));
-                                        let output4 = run(program.clone(), (i4, output3[0]));
-                                        if output4[0] > max_output.unwrap_or(output4[0] - 1) {
-                                            max_output = Some(output4[0]);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+    for perm in Permutations::from(0..=4) {
+        match perm.as_slice() {
+            [i0, i1, i2, i3, i4] => {
+                let output0 = run(program.clone(), (*i0, 0));
+                let output1 = run(program.clone(), (*i1, output0[0]));
+                let output2 = run(program.clone(), (*i2, output1[0]));
+                let output3 = run(program.clone(), (*i3, output2[0]));
+                let output4 = run(program.clone(), (*i4, output3[0]));
+                if output4[0] > max_output.unwrap_or(output4[0] - 1) {
+                    max_output = Some(output4[0]);
                 }
             }
+            _ => unreachable!(),
         }
     }
 
@@ -149,80 +139,65 @@ fn solve_a(program: Vec<i32>) -> i32 {
 fn solve_b(program: Vec<i32>) -> i32 {
     let mut max_output: Option<i32> = None;
 
-    for i1 in 5..=9 {
-        for i2 in 5..=9 {
-            if i1 != i2 {
-                for i3 in 5..=9 {
-                    if i3 != i1 && i3 != i2 {
-                        for i4 in 5..=9 {
-                            if i4 != i1 && i4 != i2 && i4 != i3 {
-                                for i5 in 5..=9 {
-                                    if i5 != i1 && i5 != i2 && i5 != i3 && i5 != i4 {
-                                        let mut eip1 = 0;
-                                        let mut eip2 = 0;
-                                        let mut eip3 = 0;
-                                        let mut eip4 = 0;
-                                        let mut eip5 = 0;
+    for perm in Permutations::from(5..=9) {
+        match perm.as_slice() {
+            [i1, i2, i3, i4, i5] => {
+                let mut eip1 = 0;
+                let mut eip2 = 0;
+                let mut eip3 = 0;
+                let mut eip4 = 0;
+                let mut eip5 = 0;
 
-                                        let mut prog1 = program.clone();
-                                        let mut prog2 = program.clone();
-                                        let mut prog3 = program.clone();
-                                        let mut prog4 = program.clone();
-                                        let mut prog5 = program.clone();
+                let mut prog1 = program.clone();
+                let mut prog2 = program.clone();
+                let mut prog3 = program.clone();
+                let mut prog4 = program.clone();
+                let mut prog5 = program.clone();
 
-                                        let mut input: Option<i32> = Some(i1);
-                                        let mut output1: Option<i32> = Some(i2);
-                                        let mut output2: Option<i32> = Some(i3);
-                                        let mut output3: Option<i32> = Some(i4);
-                                        let mut output4: Option<i32> = Some(i5);
-                                        let mut output5: Option<i32> = Some(0);
+                let mut input: Option<i32> = Some(*i1);
+                let mut output1: Option<i32> = Some(*i2);
+                let mut output2: Option<i32> = Some(*i3);
+                let mut output3: Option<i32> = Some(*i4);
+                let mut output4: Option<i32> = Some(*i5);
+                let mut output5: Option<i32> = Some(0);
 
-                                        while prog5[eip5] != 99 {
-                                            let out1 = step(
-                                                eip1,
-                                                prog1,
-                                                &mut output1,
-                                                if input.is_some() {
-                                                    &mut input
-                                                } else {
-                                                    &mut output5
-                                                },
-                                            );
-                                            eip1 = out1.0;
-                                            prog1 = out1.1;
+                while prog5[eip5] != 99 {
+                    let out1 = step(
+                        eip1,
+                        prog1,
+                        &mut output1,
+                        if input.is_some() {
+                            &mut input
+                        } else {
+                            &mut output5
+                        },
+                    );
+                    eip1 = out1.0;
+                    prog1 = out1.1;
 
-                                            let out2 =
-                                                step(eip2, prog2, &mut output2, &mut output1);
-                                            eip2 = out2.0;
-                                            prog2 = out2.1;
+                    let out2 = step(eip2, prog2, &mut output2, &mut output1);
+                    eip2 = out2.0;
+                    prog2 = out2.1;
 
-                                            let out3 =
-                                                step(eip3, prog3, &mut output3, &mut output2);
-                                            eip3 = out3.0;
-                                            prog3 = out3.1;
+                    let out3 = step(eip3, prog3, &mut output3, &mut output2);
+                    eip3 = out3.0;
+                    prog3 = out3.1;
 
-                                            let out4 =
-                                                step(eip4, prog4, &mut output4, &mut output3);
-                                            eip4 = out4.0;
-                                            prog4 = out4.1;
+                    let out4 = step(eip4, prog4, &mut output4, &mut output3);
+                    eip4 = out4.0;
+                    prog4 = out4.1;
 
-                                            let out5 =
-                                                step(eip5, prog5, &mut output5, &mut output4);
-                                            eip5 = out5.0;
-                                            prog5 = out5.1;
-                                        }
+                    let out5 = step(eip5, prog5, &mut output5, &mut output4);
+                    eip5 = out5.0;
+                    prog5 = out5.1;
+                }
 
-                                        let out = output5.take().unwrap();
-                                        if out > max_output.unwrap_or(out - 1) {
-                                            max_output = Some(out);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                let out = output5.take().unwrap();
+                if out > max_output.unwrap_or(out - 1) {
+                    max_output = Some(out);
                 }
             }
+            _ => unreachable!(),
         }
     }
 
