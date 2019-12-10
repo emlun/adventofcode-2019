be183017fe1b6520bbd6b74c9e3975a161687bdb
Emil Lundberg <emil@emlun.se>
2019-12-10 08:02:45 +0100

    Solve day 10b

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -28,8 +28,25 @@ pub fn solve(lines: &[String]) -> Solution {
         .map(|(r, c, _)| (r.try_into().unwrap(), c.try_into().unwrap()))
         .collect();
 
-    let mut visibility: HashMap<Point, usize> = HashMap::new();
     let mut max_vis = 0;
+    let mut laser_pos: Point = (0, 0);
+    let mut laser_rays: HashSet<Point> = HashSet::new();
+    let mut asteroid_rays: HashMap<Point, Vec<Point>> = HashMap::new();
+
+    fn normalize_dir(r: i64, c: i64) -> (i64, i64) {
+        if c == 0 {
+            if r == 0 {
+                unreachable!();
+            } else {
+                (r / r.abs(), 0)
+            }
+        } else if r == 0 {
+            (0, c / c.abs())
+        } else {
+            let d = gcd(r.abs(), c.abs());
+            (r / d, c / d)
+        }
+    }
 
     for (r0, c0) in &map {
         let mut recentered_map: HashSet<Point> =
@@ -40,27 +57,63 @@ pub fn solve(lines: &[String]) -> Solution {
 
         let mut visible = 0;
         let mut blocked_rays: HashSet<(i64, i64)> = HashSet::new();
-        for (r, c) in recentered_map {
-            let ray = if c == 0 {
-                if r == 0 {
-                    unreachable!();
-                } else {
-                    (r / r.abs(), 0)
-                }
-            } else if r == 0 {
-                (0, c / c.abs())
-            } else {
-                let d = gcd(r.abs(), c.abs());
-                (r / d, c / d)
-            };
+        let mut tmp_asteroid_rays: HashMap<Point, Vec<Point>> = HashMap::new();
+        for (r, c) in recentered_map.iter() {
+            let ray = normalize_dir(*r, *c);
 
             if !blocked_rays.contains(&ray) {
                 visible += 1;
+                tmp_asteroid_rays.insert(ray, Vec::new());
             }
+            tmp_asteroid_rays.get_mut(&ray).unwrap().push((*r, *c));
             blocked_rays.insert(ray);
         }
-        max_vis = std::cmp::max(max_vis, visible);
+        if visible > max_vis {
+            max_vis = visible;
+            laser_pos = (*r0, *c0);
+            laser_rays = blocked_rays;
+            asteroid_rays = tmp_asteroid_rays;
+        }
     }
 
-    (max_vis.to_string(), "bar".to_string())
+    fn ray_atan((r, c): &Point) -> f64 {
+        ((*c as f64).atan2(-*r as f64) + 2.0 * std::f64::consts::PI) % (2.0 * std::f64::consts::PI)
+    }
+
+    let mut laser_rays: Vec<Point> = laser_rays.into_iter().collect();
+    laser_rays.sort_by(|ray1, ray2| {
+        let diff = ray_atan(ray1) - ray_atan(ray2);
+        if diff < 0.0 {
+            std::cmp::Ordering::Less
+        } else {
+            std::cmp::Ordering::Greater
+        }
+    });
+
+    let mut asteroid_rays: Vec<Vec<Point>> = laser_rays
+        .into_iter()
+        .map(|ray| asteroid_rays.remove(&ray).unwrap())
+        .collect();
+
+    let mut num = 0;
+    let mut i = 0;
+    let mut b_solution = 0;
+    while !asteroid_rays.is_empty() {
+        let ast = asteroid_rays[i].remove(0);
+        let orig_ast = (ast.0 + laser_pos.0, ast.1 + laser_pos.1);
+        i += 1;
+        num += 1;
+        if num == 200 {
+            b_solution = orig_ast.0 + orig_ast.1 * 100;
+        }
+        if i >= asteroid_rays.len() {
+            i -= asteroid_rays.len();
+            asteroid_rays = asteroid_rays
+                .into_iter()
+                .filter(|v| !v.is_empty())
+                .collect();
+        }
+    }
+
+    (max_vis.to_string(), b_solution.to_string())
 }
