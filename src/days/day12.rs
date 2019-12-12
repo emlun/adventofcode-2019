902d617bbf479119a31d9e8de1757a3d23de73e7
Emil Lundberg <emil@emlun.se>
2019-12-12 06:54:24 +0100

    Solve day 12b

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,8 +1,8 @@
 use crate::common::Solution;
+use std::collections::HashSet;
 
 type Point = (i64, i64, i64);
 
-#[derive(Debug)]
 struct Moon {
     pos: Point,
     vel: Point,
@@ -49,7 +49,26 @@ fn step(mut moons: Vec<Moon>) -> Vec<Moon> {
     moons
 }
 
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: i64, b: i64, c: i64) -> i64 {
+    let gcdab = gcd(a, b);
+    let lcmab = (a / gcdab) * b;
+    let gcdabc = gcd(lcmab, c);
+    (lcmab / gcdabc) * c
+}
+
 pub fn solve(lines: &[String]) -> Solution {
+    let mut x_states: HashSet<Vec<(i64, i64)>> = HashSet::new();
+    let mut y_states: HashSet<Vec<(i64, i64)>> = HashSet::new();
+    let mut z_states: HashSet<Vec<(i64, i64)>> = HashSet::new();
+
     let mut moons: Vec<Moon> = lines
         .iter()
         .map(|line| {
@@ -73,9 +92,54 @@ pub fn solve(lines: &[String]) -> Solution {
             }
         })
         .collect();
+
+    let mut b_solution: Option<i64> = None;
+
     for _ in 0..1000 {
         moons = step(moons);
+        x_states.insert(moons.iter().map(|moon| (moon.pos.0, moon.vel.0)).collect());
+        y_states.insert(moons.iter().map(|moon| (moon.pos.1, moon.vel.1)).collect());
+        z_states.insert(moons.iter().map(|moon| (moon.pos.2, moon.vel.2)).collect());
     }
+
     let a_solution: i64 = moons.iter().map(energy).sum();
-    (a_solution.to_string(), "bar".to_string())
+
+    let mut x_cycle = None;
+    let mut y_cycle = None;
+    let mut z_cycle = None;
+
+    for i in 1000.. {
+        moons = step(moons);
+
+        if x_cycle.is_none() {
+            let x_state = moons.iter().map(|moon| (moon.pos.0, moon.vel.0)).collect();
+            if x_states.contains(&x_state) {
+                x_cycle = Some(i);
+            }
+            x_states.insert(x_state);
+        }
+
+        if y_cycle.is_none() {
+            let y_state = moons.iter().map(|moon| (moon.pos.1, moon.vel.1)).collect();
+            if y_states.contains(&y_state) {
+                y_cycle = Some(i);
+            }
+            y_states.insert(y_state);
+        }
+
+        if z_cycle.is_none() {
+            let z_state = moons.iter().map(|moon| (moon.pos.2, moon.vel.2)).collect();
+            if z_states.contains(&z_state) {
+                z_cycle = Some(i);
+            }
+            z_states.insert(z_state);
+        }
+
+        if let (Some(xc), Some(yc), Some(zc)) = (x_cycle, y_cycle, z_cycle) {
+            b_solution = Some(lcm(xc, yc, zc));
+            break;
+        }
+    }
+
+    (a_solution.to_string(), b_solution.unwrap().to_string())
 }
