b460a4ec6ac3ea658e987beb0f0b38286d8fc15f
Emil Lundberg <emil@emlun.se>
2019-12-13 06:27:50 +0100

    Solve day 13b

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -5,6 +5,8 @@ use std::convert::TryInto;
 
 type Point = (i64, i64);
 
+const ENABLE_OUTPUT: bool = false;
+
 fn step(
     eip: usize,
     mut prog: Vec<i64>,
@@ -174,7 +176,109 @@ fn solve_a(mut program: Vec<i64>) -> String {
         .to_string()
 }
 
+fn sign(i: i64) -> i64 {
+    if i == 0 {
+        0
+    } else {
+        i / i.abs()
+    }
+}
+
+fn solve_b(mut program: Vec<i64>) -> String {
+    let mut world: HashMap<Point, Tile> = HashMap::new();
+    let mut eip = 0;
+    let mut relbase = 0;
+    let mut state = 0;
+    let mut output_x = 0;
+    let mut output_y = 0;
+    let mut joystick = 0;
+    let mut score = 0;
+    let mut paddle_x = 0;
+    let mut ball_x = 0;
+
+    program[0] = 2;
+
+    loop {
+        let mut output: Option<i64> = None;
+        let mut input: Option<i64> = Some(joystick);
+        let (eip2, prog2) = step(eip, program, &mut relbase, &mut output, &mut input);
+        eip = eip2;
+        program = prog2;
+        if let Some(out) = output {
+            match state {
+                0 => {
+                    output_x = out;
+                }
+                1 => {
+                    output_y = out;
+                }
+                2 => {
+                    if (output_x, output_y) == (-1, 0) {
+                        score = out;
+                    } else {
+                        let output_tile_id = match out {
+                            0 => Tile::Empty,
+                            1 => Tile::Wall,
+                            2 => Tile::Block,
+                            3 => Tile::Paddle,
+                            4 => Tile::Ball,
+                            _ => unreachable!(),
+                        };
+                        if output_tile_id == Tile::Paddle {
+                            paddle_x = output_x;
+                        }
+                        if output_tile_id == Tile::Ball {
+                            ball_x = output_x;
+                        }
+                        world.insert((output_x, output_y), output_tile_id);
+                    }
+                }
+                _ => unreachable!(),
+            };
+
+            state += 1;
+            state %= 3;
+
+            if ENABLE_OUTPUT {
+                let minx = *world.keys().map(|(x, _)| x).min().unwrap_or(&0);
+                let maxx = *world.keys().map(|(x, _)| x).max().unwrap_or(&0);
+                let miny = *world.keys().map(|(_, y)| y).min().unwrap_or(&0);
+                let maxy = *world.keys().map(|(_, y)| y).max().unwrap_or(&0);
+
+                println!(
+                    "\n{}\n{}",
+                    score,
+                    (miny..=maxy)
+                        .rev()
+                        .map(|y| {
+                            (minx..=maxx)
+                                .map(|x| match *world.get(&(x, y)).unwrap_or(&Tile::Empty) {
+                                    Tile::Empty => " ",
+                                    Tile::Wall => "#",
+                                    Tile::Block => "B",
+                                    Tile::Paddle => "-",
+                                    Tile::Ball => "o",
+                                    _ => unreachable!(),
+                                })
+                                .collect::<Vec<&str>>()
+                                .join("")
+                        })
+                        .collect::<Vec<String>>()
+                        .join("\n")
+                );
+            }
+
+            joystick = sign(ball_x - paddle_x);
+        }
+        if program[eip] == 99 {
+            break;
+        }
+    }
+
+    score.to_string()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let program: Vec<i64> = lines[0].split(',').map(|s| s.parse().unwrap()).collect();
-    (solve_a(program.clone()).to_string(), solve_a(program))
+    (solve_a(program.clone()).to_string(), solve_b(program))
 }
