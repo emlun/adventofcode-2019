8ef9fec5edb042c79020aaf395fd30c3d30aaf1b
Emil Lundberg <emil@emlun.se>
2019-12-13 06:33:15 +0100

    Add day 13 to tests

--- tests/test.rs
+++ tests/test.rs
@@ -41,7 +41,7 @@ fn lookup(day: u8) -> Option<(fn(&[String]) -> Solution, (&'static str, &'static
             ),
         )),
         12 => Some((days::day12::solve, ("5937", "376203951569712"))),
-        // 13 => Some((days::day13::solve, ("", ""))),
+        13 => Some((days::day13::solve, ("200", "9803"))),
         // 14 => Some((days::day14::solve, ("", ""))),
         // 15 => Some((days::day15::solve, ("", ""))),
         // 16 => Some((days::day16::solve, ("", ""))),
