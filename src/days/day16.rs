64766417b3cf8a8cc63d4005859a7d7dd2a457e6
Emil Lundberg <emil@emlun.se>
2019-12-16 09:44:45 +0100

    Move functions out from solve()

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -11,38 +11,38 @@ fn sum_term(i: usize, digits: &Vec<i8>, pattern_num: usize) -> i32 {
     (digits[i] * pattern_digit(pattern_num, i)) as i32
 }
 
+fn phase_digit(digits: &Vec<i8>, n: usize) -> i8 {
+    // println!(
+    //     "\n{} {:?} {:?}",
+    //     n,
+    //     digits,
+    //     pattern(n).take(digits.len()).collect::<Vec<i8>>()
+    // );
+    let d = (0..digits.len())
+        .map(|i| sum_term(i, digits, n + 1))
+        .sum::<i32>()
+        .abs()
+        % 10;
+    // dbg!(d)
+    d as i8
+}
+
+fn phase(digits: &Vec<i8>) -> Vec<i8> {
+    (0..digits.len()).map(|i| phase_digit(digits, i)).collect()
+}
+
+fn transform(digits: &Vec<i8>, phases: usize) -> Vec<i8> {
+    (0..phases).fold(digits.clone(), |digs, _| phase(&digs))
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let digits: Vec<i8> = lines[0].chars().map(|c| (c as i8) - 48).collect();
 
     println!("{:?}", digits);
 
-    fn phase_digit(digits: &Vec<i8>, n: usize) -> i8 {
-        // println!(
-        //     "\n{} {:?} {:?}",
-        //     n,
-        //     digits,
-        //     pattern(n).take(digits.len()).collect::<Vec<i8>>()
-        // );
-        let d = (0..digits.len())
-            .map(|i| sum_term(i, digits, n + 1))
-            .sum::<i32>()
-            .abs()
-            % 10;
-        // dbg!(d)
-        d as i8
-    }
-
-    fn phase(digits: &Vec<i8>) -> Vec<i8> {
-        (0..digits.len()).map(|i| phase_digit(digits, i)).collect()
-    };
-
     println!("{:?}", phase(&digits));
     println!("{:?}", phase(&phase(&digits)));
     println!("{:?}", phase(&phase(&phase(&digits))));
-
-    fn transform(digits: &Vec<i8>, phases: usize) -> Vec<i8> {
-        (0..phases).fold(digits.clone(), |digs, _| phase(&digs))
-    }
 
     let a_solution = transform(&digits, 100)
         .into_iter()
