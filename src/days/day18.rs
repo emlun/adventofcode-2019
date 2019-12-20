eab821cf72e2a4fcc271241bbd12192354bad6b8
Emil Lundberg <emil@emlun.se>
2019-12-20 22:27:55 +0100

    Use custom KeySet bit flag type for day 18

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,10 +1,8 @@
 use crate::common::Solution;
-use std::collections::BTreeSet;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
-use std::rc::Rc;
 
 type Point = (usize, usize);
 
@@ -18,8 +16,8 @@ fn adjacent(pos: &Point) -> Vec<Point> {
 }
 
 enum Entity {
-    Key(char),
-    Door(char),
+    Key(KeyId),
+    Door(KeyId),
 }
 
 enum Tile {
@@ -30,10 +28,64 @@ enum Tile {
 use Entity::{Door, Key};
 use Tile::{Floor, Wall};
 
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct KeyId {
+    value: u32,
+}
+
+impl KeyId {
+    fn to_char(&self) -> char {
+        ((f64::from(self.value).log2().round() as u8) + ('a' as u8)) as char
+    }
+}
+
+impl From<char> for KeyId {
+    fn from(c: char) -> Self {
+        let basis = if c.is_ascii_uppercase() { 'A' } else { 'a' };
+        KeyId {
+            value: 2_u32.pow(c as u32 - basis as u32),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct KeySet {
+    keys: u32,
+}
+
+impl KeySet {
+    fn insert(&mut self, key: KeyId) -> &mut Self {
+        self.keys |= key.value;
+        self
+    }
+
+    fn contains(&self, key: KeyId) -> bool {
+        self.keys & key.value != 0
+    }
+
+    fn len_equals(&self, len: u32) -> bool {
+        self.keys == 2_u32.pow(len) - 1
+    }
+}
+
+impl<K> std::iter::FromIterator<K> for KeySet
+where
+    K: Into<KeyId>,
+{
+    fn from_iter<I>(it: I) -> Self
+    where
+        I: IntoIterator<Item = K>,
+    {
+        KeySet {
+            keys: it.into_iter().fold(0, |keys, key| keys | key.into().value),
+        }
+    }
+}
+
 #[derive(Debug, Eq, Ord, PartialEq)]
 struct State {
     pub poss: Vec<Point>,
-    pub collected: Rc<BTreeSet<char>>,
+    pub collected: KeySet,
     pub len: usize,
 }
 
@@ -45,7 +97,7 @@ impl PartialOrd for State {
 
 struct World {
     tiles: Vec<Vec<Tile>>,
-    keys: HashMap<Point, char>,
+    keys: HashMap<Point, KeyId>,
 }
 
 impl World {
@@ -54,7 +106,7 @@ impl World {
         for (r, row) in tiles.iter().enumerate() {
             for (c, tile) in row.iter().enumerate() {
                 if let Floor(Some(Key(k))) = tile {
-                    keys.insert((c, r), *k);
+                    keys.insert((c, r), KeyId::from(*k));
                 }
             }
         }
@@ -64,21 +116,21 @@ impl World {
 
 fn compute_transfers(
     world: &World,
-    collected: &BTreeSet<char>,
-) -> HashMap<Point, Vec<(Point, usize, char)>> {
-    let mut result: HashMap<Point, Vec<(Point, usize, char)>> = HashMap::new();
+    collected: KeySet,
+) -> HashMap<Point, Vec<(Point, usize, KeyId)>> {
+    let mut result: HashMap<Point, Vec<(Point, usize, KeyId)>> = HashMap::new();
 
     for start_pos in world.keys.keys() {
-        let mut partmap: Vec<(Point, usize, char)> = Vec::new();
+        let mut partmap: Vec<(Point, usize, KeyId)> = Vec::new();
         let mut visited = HashSet::new();
         let mut queue: VecDeque<(Point, usize)> = VecDeque::new();
         queue.push_back((*start_pos, 0));
 
         while let Some((step, len)) = queue.pop_front() {
             for next in adjacent(&step) {
-                if !visited.contains(&next) && can_walk(world, &collected, &next) {
+                if !visited.contains(&next) && can_walk(world, collected, &next) {
                     if let Floor(Some(Key(k))) = world.tiles[next.1][next.0] {
-                        if !collected.contains(&k) {
+                        if !collected.contains(k) {
                             partmap.push((next, len + 1, k));
                         } else {
                             queue.push_back((next, len + 1));
@@ -111,12 +163,12 @@ fn parse_world(lines: &[String]) -> (World, Point) {
                     '.' => Floor(None),
                     '@' => {
                         player_pos = (x, y);
-                        Floor(Some(Key('@')))
+                        Floor(Some(Key('{'.into())))
                     }
                     a => Floor(Some(if a == a.to_ascii_uppercase() {
-                        Door(a.to_ascii_lowercase())
+                        Door(a.into())
                     } else {
-                        Key(a)
+                        Key(a.into())
                     })),
                 })
                 .collect()
@@ -125,39 +177,33 @@ fn parse_world(lines: &[String]) -> (World, Point) {
     (World::new(world), player_pos)
 }
 
-fn can_walk(world: &World, collected: &BTreeSet<char>, pos: &Point) -> bool {
+fn can_walk(world: &World, collected: KeySet, pos: &Point) -> bool {
     match world.tiles[pos.1][pos.0] {
         Wall => false,
         Floor(None) => true,
         Floor(Some(Key(_))) => true,
-        Floor(Some(Door(a))) => collected.contains(&a),
+        Floor(Some(Door(a))) => collected.contains(a),
     }
 }
 
-fn dijkstra(
-    world: &World,
-    start_positions: &Vec<Point>,
-    start_collected: BTreeSet<char>,
-) -> Option<State> {
-    let mut transfers: HashMap<Rc<BTreeSet<char>>, HashMap<Point, Vec<(Point, usize, char)>>> =
-        HashMap::new();
+fn dijkstra(world: &World, start_positions: &Vec<Point>, start_collected: KeySet) -> Option<State> {
+    let mut transfers: HashMap<KeySet, HashMap<Point, Vec<(Point, usize, KeyId)>>> = HashMap::new();
     let mut queue: BinaryHeap<State> = BinaryHeap::new();
 
-    let mut shortest_collections: HashMap<Rc<BTreeSet<char>>, HashMap<char, usize>> =
-        HashMap::new();
+    let mut shortest_collections: HashMap<KeySet, HashMap<KeyId, usize>> = HashMap::new();
 
     queue.push(State {
         poss: start_positions.clone(),
-        collected: Rc::new(start_collected),
+        collected: start_collected,
         len: 0,
     });
 
     while let Some(state) = queue.pop() {
-        if state.collected.len() == world.keys.len() {
+        if state.collected.len_equals(world.keys.len() as u32) {
             return Some(state);
         } else {
             for posi in 0..state.poss.len() {
-                let last_key: &char = world.keys.get(&state.poss[posi]).unwrap();
+                let last_key: &KeyId = world.keys.get(&state.poss[posi]).unwrap();
 
                 if shortest_collections
                     .get(&state.collected)
@@ -168,13 +214,13 @@ fn dijkstra(
                     let trns = if let Some(t) = transfers.get(&state.collected) {
                         t
                     } else {
-                        let t = compute_transfers(world, &state.collected);
-                        transfers.insert(Rc::clone(&state.collected), t);
+                        let t = compute_transfers(world, state.collected);
+                        transfers.insert(state.collected, t);
                         transfers.get(&state.collected).unwrap()
                     };
 
                     let shortcoll = shortest_collections
-                        .entry(Rc::clone(&state.collected))
+                        .entry(state.collected)
                         .or_insert(HashMap::new())
                         .entry(*last_key)
                         .or_insert(state.len);
@@ -185,8 +231,8 @@ fn dijkstra(
 
                     for (next_point, len_to_next, next_key) in trns.get(&state.poss[posi]).unwrap()
                     {
-                        let mut collected = Rc::clone(&state.collected);
-                        Rc::make_mut(&mut collected).insert(*next_key);
+                        let mut collected = state.collected;
+                        collected.insert(*next_key);
 
                         let mut poss = state.poss.clone();
                         poss[posi] = *next_point;
@@ -207,7 +253,7 @@ fn dijkstra(
 }
 
 fn solve_a(world: &World, pos: Vec<Point>) -> usize {
-    let found = dijkstra(world, &pos, vec!['@'].into_iter().collect());
+    let found = dijkstra(world, &pos, vec!['{'].into_iter().collect());
     found.unwrap().len
 }
 
@@ -218,10 +264,10 @@ fn solve_b(mut world: World, pos: Point) -> usize {
     world.tiles[pos.1 + 1][pos.0] = Tile::Wall;
     world.tiles[pos.1][pos.0 + 1] = Tile::Wall;
     world.keys.remove(&pos);
-    world.keys.insert((pos.0 - 1, pos.1 - 1), '@');
-    world.keys.insert((pos.0 - 1, pos.1 + 1), '#');
-    world.keys.insert((pos.0 + 1, pos.1 + 1), '$');
-    world.keys.insert((pos.0 + 1, pos.1 - 1), '%');
+    world.keys.insert((pos.0 - 1, pos.1 - 1), '{'.into());
+    world.keys.insert((pos.0 - 1, pos.1 + 1), '|'.into());
+    world.keys.insert((pos.0 + 1, pos.1 + 1), '}'.into());
+    world.keys.insert((pos.0 + 1, pos.1 - 1), '~'.into());
 
     let pos = vec![
         (pos.0 - 1, pos.1 - 1),
@@ -230,7 +276,7 @@ fn solve_b(mut world: World, pos: Point) -> usize {
         (pos.0 + 1, pos.1 - 1),
     ];
 
-    let found = dijkstra(&world, &pos, vec!['@', '#', '$', '%'].into_iter().collect());
+    let found = dijkstra(&world, &pos, vec!['{', '|', '}', '~'].into_iter().collect());
     found.unwrap().len
 }
 
@@ -245,8 +291,8 @@ fn print_world(world: &World) {
                 .iter()
                 .map(|c| match c {
                     Wall => '#'.to_string(),
-                    Floor(Some(Key(a))) => a.to_string(),
-                    Floor(Some(Door(a))) => a.to_ascii_uppercase().to_string(),
+                    Floor(Some(Key(a))) => a.to_char().to_string(),
+                    Floor(Some(Door(a))) => a.to_char().to_ascii_uppercase().to_string(),
                     Floor(None) => '.'.to_string(),
                 })
                 .collect::<Vec<String>>()
@@ -272,8 +318,8 @@ fn print_state(world: &World, state: &State) {
                 } else {
                     match tile {
                         Wall => '#'.to_string(),
-                        Floor(Some(Key(a))) => a.to_string(),
-                        Floor(Some(Door(a))) => a.to_ascii_uppercase().to_string(),
+                        Floor(Some(Key(a))) => a.to_char().to_string(),
+                        Floor(Some(Door(a))) => a.to_char().to_ascii_uppercase().to_string(),
                         Floor(None) => '.'.to_string(),
                     }
                 })
