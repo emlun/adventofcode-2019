bb976186e3df929506dd4efd234ed47eb99c017d
Emil Lundberg <emil@emlun.se>
2019-12-23 07:29:06 +0100

    Add day 23 to benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -118,6 +118,11 @@ fn day21(b: &mut Bencher) {
     run_bench(21, b);
 }
 
+#[bench]
+fn day23(b: &mut Bencher) {
+    run_bench(23, b);
+}
+
 // #[bench]
 // fn days_all(b: &mut Bencher) {
 //     let solvers_and_inputs: Vec<(fn(&[String]) -> Solution, Vec<String>)> = days::all_numbers()
