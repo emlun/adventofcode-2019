a3abf1c56db71d8dddc1417751d2990ecb3efde6
Emil Lundberg <emil@emlun.se>
2019-12-24 09:10:50 +0100

    Solve day 24b

--- src/days/day24.rs
+++ src/days/day24.rs
@@ -1,19 +1,23 @@
 use crate::common::Solution;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn parse(lines: &[String]) -> Vec<Vec<bool>> {
-    [".....".to_string()]
+    lines
         .iter()
-        .chain(lines.iter())
-        .chain([".....".to_string()].iter())
-        .map(|line| {
-            ".".chars()
-                .chain(line.chars())
-                .chain(".".chars())
-                .map(|c| c == '#')
-                .collect()
-        })
+        .map(|line| line.chars().map(|c| c == '#').collect())
         .collect()
+}
+
+fn parse_a(lines: &[String]) -> Vec<Vec<bool>> {
+    let mut state = parse(lines);
+    for row in state.iter_mut() {
+        row.insert(0, false);
+        row.push(false);
+    }
+    state.insert(0, vec![false; 7]);
+    state.push(vec![false; 7]);
+    state
 }
 
 fn print_state(state: &Vec<Vec<bool>>) {
@@ -28,6 +32,26 @@ fn print_state(state: &Vec<Vec<bool>>) {
         .collect::<Vec<String>>()
         .join("\n");
     println!("{}", s);
+}
+
+fn print_levels(state: &HashMap<i32, Vec<Vec<bool>>>) {
+    let minl = *state.keys().min().unwrap();
+    let maxl = *state.keys().max().unwrap();
+    for level in minl..=maxl {
+        let s: String = state
+            .get(&level)
+            .unwrap()
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|c| if *c { "#" } else { "." }.to_string())
+                    .collect::<Vec<String>>()
+                    .join("")
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+        println!("\nLevel {}:\n{}", level, s);
+    }
 }
 
 fn score(state: &Vec<Vec<bool>>) -> u128 {
@@ -66,8 +90,99 @@ fn update(
     (next_state, state)
 }
 
+fn empty_level() -> Vec<Vec<bool>> {
+    vec![vec![false; 7]; 7]
+}
+
+fn update_b(
+    mut state: HashMap<i32, Vec<Vec<bool>>>,
+    mut next_state: HashMap<i32, Vec<Vec<bool>>>,
+) -> (HashMap<i32, Vec<Vec<bool>>>, HashMap<i32, Vec<Vec<bool>>>) {
+    let maxi = 5;
+    let lmin = state.keys().min().unwrap();
+    let lmax = state.keys().max().unwrap();
+    let empty_lvl = empty_level();
+    for level in (lmin - 1)..=(lmax + 1) {
+        for y in 1..=maxi {
+            for x in 1..=maxi {
+                if x == 3 && y == 3 {
+                    continue;
+                }
+
+                fn count_neighbors(
+                    state: &HashMap<i32, Vec<Vec<bool>>>,
+                    empty_lvl: &Vec<Vec<bool>>,
+                    of_x: usize,
+                    of_y: usize,
+                    of_level: i32,
+                    at_x: usize,
+                    at_y: usize,
+                ) -> usize {
+                    let lv = if at_x == 3 && at_y == 3 {
+                        of_level + 1
+                    } else if at_x == 0 || at_x == 6 || at_y == 0 || at_y == 6 {
+                        of_level - 1
+                    } else {
+                        of_level
+                    };
+                    let lvl = state.get(&lv).unwrap_or(empty_lvl);
+
+                    if at_x == 3 && at_y == 3 {
+                        if of_x == 2 {
+                            (1..=5).filter(|y| lvl[*y][1]).count()
+                        } else if of_x == 4 {
+                            (1..=5).filter(|y| lvl[*y][5]).count()
+                        } else if of_y == 2 {
+                            (1..=5).filter(|x| lvl[1][*x]).count()
+                        } else if of_y == 4 {
+                            (1..=5).filter(|x| lvl[5][*x]).count()
+                        } else {
+                            unreachable!()
+                        }
+                    } else {
+                        let cell = if at_x == 0 {
+                            lvl[3][2]
+                        } else if at_x == 6 {
+                            lvl[3][4]
+                        } else if at_y == 0 {
+                            lvl[2][3]
+                        } else if at_y == 6 {
+                            lvl[4][3]
+                        } else {
+                            lvl[at_y][at_x]
+                        };
+                        if cell {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                }
+
+                let neighbors: usize = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+                    .iter()
+                    .map(|(x2, y2)| count_neighbors(&state, &empty_lvl, x, y, level, *x2, *y2))
+                    .sum();
+                // println!(
+                //     "Cell {}:{:?} has {} live neighbors",
+                //     level,
+                //     (x, y),
+                //     neighbors
+                // );
+                next_state.entry(level).or_insert_with(empty_level)[y][x] =
+                    if state.get(&level).unwrap_or(&empty_lvl)[y][x] {
+                        neighbors == 1
+                    } else {
+                        neighbors == 1 || neighbors == 2
+                    };
+            }
+        }
+    }
+    (next_state, state)
+}
+
 fn solve_a(lines: &[String]) -> u128 {
-    let initial_state = parse(lines);
+    let initial_state = parse_a(lines);
     print_state(&initial_state);
 
     let mut state = initial_state.clone();
@@ -87,14 +202,30 @@ fn solve_a(lines: &[String]) -> u128 {
     }
 }
 
-// fn solve_b(lines: &[String]) -> u128 {
-//     let poly = Deck::polynomial_for(119315717514047, lines);
-//     poly.self_composed_deg1(101741582076661).apply(2020)
-// }
+fn solve_b(lines: &[String]) -> usize {
+    let initial_state = parse_a(lines);
+    let mut state: HashMap<i32, Vec<Vec<bool>>> = HashMap::new();
+    state.insert(0, initial_state);
+    let mut tmp = state.clone();
+
+    for _ in 0..200 {
+        println!();
+        print_levels(&state);
+        let o = update_b(state, tmp);
+        state = o.0;
+        tmp = o.1;
+    }
+
+    state
+        .values()
+        .flat_map(|grid| grid.iter())
+        .flat_map(|row| row.iter())
+        .filter(|v| **v)
+        .count()
+}
 
 pub fn solve(lines: &[String]) -> Solution {
     let a_solution = solve_a(lines);
-    // let b_solution = solve_b(lines);
-    let b_solution = "bar";
+    let b_solution = solve_b(lines);
     (a_solution.to_string(), b_solution.to_string())
 }
