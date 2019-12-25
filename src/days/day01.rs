1cbe4dea2813b865a24e2f6a12fcf0bf856cd063
Emil Lundberg <emil@emlun.se>
2019-12-25 22:23:20 +0100

    Optimize day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,10 +1,10 @@
 use crate::common::Solution;
 
-fn base_fuel_mass(mass: i64) -> i64 {
+fn base_fuel_mass(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
-fn full_fuel_mass(mass: i64) -> i64 {
+fn full_fuel_mass(mass: i32) -> i32 {
     let m = base_fuel_mass(mass);
     if m > 0 {
         m + full_fuel_mass(m)
@@ -14,11 +14,13 @@ fn full_fuel_mass(mass: i64) -> i64 {
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let module_masses: Vec<i64> = lines
-        .iter()
-        .map(|line| line.parse::<i64>().unwrap())
-        .collect();
-    let a_solution: i64 = module_masses.iter().map(|i| base_fuel_mass(*i)).sum();
-    let b_solution: i64 = module_masses.iter().map(|i| full_fuel_mass(*i)).sum();
+    let (a_solution, b_solution) =
+        lines
+            .iter()
+            .map(|line| line.parse::<i32>().unwrap())
+            .fold((0, 0), |(suma, sumb), i| {
+                let base = base_fuel_mass(i);
+                (suma + base, sumb + base + full_fuel_mass(base))
+            });
     (a_solution.to_string(), b_solution.to_string())
 }
