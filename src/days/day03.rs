5e368f92832ac71cd9be1c296e04316522dcde67
Emil Lundberg <emil@emlun.se>
2020-08-17 23:50:08 +0200

    Represent points as i64 instead of (i64, i64)

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -3,15 +3,27 @@ use std::collections::HashSet;
 
 use crate::common::Solution;
 
-type Point = (i64, i64);
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Point(i64);
+impl Point {
+    fn new((x, y): (i32, i32)) -> Point {
+        let slf = Point(((x as i64) << 32) | (y as i64 & 0xffffffff));
+        debug_assert_eq!((x, y), slf.as_tuple(), "{:?}", slf);
+        slf
+    }
 
-fn norm(p: &&&Point) -> u64 {
-    (p.0.abs() as u64) + (p.1.abs() as u64)
+    fn as_tuple(&self) -> (i32, i32) {
+        ((self.0 >> 32) as i32, (self.0 & 0xffffffff) as i32)
+    }
+
+    fn norm(&self) -> i32 {
+        ((self.0 >> 32) as i32).abs() + ((self.0 & 0xffffffff) as i32).abs()
+    }
 }
 
 fn parse_wire(desc: &str) -> Vec<Point> {
     let mut points: Vec<Point> = Vec::new();
-    let mut pos: Point = (0, 0);
+    let mut pos = (0, 0);
     for step in desc.split(',') {
         let dir = match step.chars().next().unwrap() {
             'R' => (1, 0),
@@ -23,7 +35,7 @@ fn parse_wire(desc: &str) -> Vec<Point> {
         let len: u32 = step[1..].parse().unwrap();
         for _i in 0..len {
             pos = (pos.0 + dir.0, pos.1 + dir.1);
-            points.push(pos);
+            points.push(Point::new(pos));
         }
     }
     points
@@ -41,7 +53,7 @@ pub fn solve(lines: &[String]) -> Solution {
 
     let intersections: HashSet<&&Point> = wire1_set.intersection(&wire2_set).collect();
 
-    let a_solution: u64 = intersections.iter().map(norm).min().unwrap();
+    let a_solution: i32 = intersections.iter().map(|p| p.norm()).min().unwrap();
     let b_solution: usize = 2 + intersections
         .iter()
         .map(|p| wire1_inv[*p] + wire2_inv[*p])
