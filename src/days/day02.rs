c6bb9b2bcb5fa7972575bd8ff0de48e6538feab0
Emil Lundberg <emil@emlun.se>
2020-08-18 02:30:56 +0200

    Use input limits defined for day 2b

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,7 +1,6 @@
 use crate::common::Solution;
 use crate::intcode::parse_program;
 use crate::intcode::IntcodeComputer;
-use std::convert::TryInto;
 
 fn solve_a(mut computer: IntcodeComputer) -> i64 {
     computer.prog[1] = 12;
@@ -13,11 +12,9 @@ fn solve_a(mut computer: IntcodeComputer) -> i64 {
 const B_OUTPUT_TARGET: i64 = 19690720;
 
 fn solve_b(program: Vec<i64>) -> i64 {
-    for noun in 0..program.len() {
-        for verb in 0..program.len() {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
             let mut prog = program.clone();
-            let noun = noun.try_into().unwrap();
-            let verb = verb.try_into().unwrap();
             prog[1] = noun;
             prog[2] = verb;
             if IntcodeComputer::new(prog).run(None).prog[0] == B_OUTPUT_TARGET {
