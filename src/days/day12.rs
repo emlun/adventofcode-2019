e70d0cab9911a7d42b35a87356a0e80254f14b31
Emil Lundberg <emil@emlun.se>
2020-08-18 04:35:09 +0200

    De-duplicate code in day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -30,7 +30,7 @@ fn step(mut moons: Vec<Moon>) -> Vec<Moon> {
     moons
 }
 
-fn gcd(a: i64, b: i64) -> i64 {
+fn gcd(a: usize, b: usize) -> usize {
     if b == 0 {
         a
     } else {
@@ -38,17 +38,26 @@ fn gcd(a: i64, b: i64) -> i64 {
     }
 }
 
-fn lcm(a: i64, b: i64, c: i64) -> i64 {
+fn lcm(a: usize, b: usize, c: usize) -> usize {
     let gcdab = gcd(a, b);
     let lcmab = (a / gcdab) * b;
     let gcdabc = gcd(lcmab, c);
     (lcmab / gcdabc) * c
 }
 
+fn find_period(initial_state: Vec<Moon>, mut moons: Vec<Moon>) -> usize {
+    for i in 1001.. {
+        moons = step(moons);
+
+        if initial_state == moons {
+            return i;
+        }
+    }
+    unreachable!();
+}
+
 pub fn solve(lines: &[String]) -> Solution {
-    let mut moons_x: Vec<Moon> = Vec::new();
-    let mut moons_y: Vec<Moon> = Vec::new();
-    let mut moons_z: Vec<Moon> = Vec::new();
+    let mut moons: Vec<Vec<Moon>> = vec![Vec::new(); 3];
     for line in lines {
         let mut pos = line.split(',').map(|part| {
             part.split('=')
@@ -61,73 +70,35 @@ pub fn solve(lines: &[String]) -> Solution {
                 .unwrap()
         });
 
-        moons_x.push(Moon {
-            pos: pos.next().unwrap(),
-            vel: 0,
-        });
-
-        moons_y.push(Moon {
-            pos: pos.next().unwrap(),
-            vel: 0,
-        });
-
-        moons_z.push(Moon {
-            pos: pos.next().unwrap(),
-            vel: 0,
-        });
+        for m in moons.iter_mut() {
+            m.push(Moon {
+                pos: pos.next().unwrap(),
+                vel: 0,
+            });
+        }
     }
 
-    let initial_x_state: Vec<Moon> = moons_x.clone();
-    let initial_y_state: Vec<Moon> = moons_y.clone();
-    let initial_z_state: Vec<Moon> = moons_z.clone();
+    let initial_states = moons.clone();
 
     for _ in 0..1000 {
-        moons_x = step(moons_x);
-        moons_y = step(moons_y);
-        moons_z = step(moons_z);
+        moons = moons.into_iter().map(step).collect();
     }
 
-    let a_solution: i64 = moons_x
+    let a_solution: i64 = moons[0]
         .iter()
-        .zip(moons_y.iter())
-        .zip(moons_z.iter())
+        .zip(moons[1].iter())
+        .zip(moons[2].iter())
         .map(|((x, y), z)| energy(x, y, z))
         .sum();
 
-    let mut x_period = None;
-    let mut y_period = None;
-    let mut z_period = None;
+    let periods: Vec<usize> = initial_states
+        .into_iter()
+        .zip(moons.into_iter())
+        .map(|(i, s)| find_period(i, s))
+        .collect();
 
-    for i in 1001.. {
-        moons_x = step(moons_x);
-
-        if initial_x_state == moons_x {
-            x_period = Some(i);
-            break;
-        }
-    }
-
-    for i in 1001.. {
-        moons_y = step(moons_y);
-
-        if initial_y_state == moons_y {
-            y_period = Some(i);
-            break;
-        }
-    }
-
-    for i in 1001.. {
-        moons_z = step(moons_z);
-
-        if initial_z_state == moons_z {
-            z_period = Some(i);
-            break;
-        }
-    }
-
-    if let (Some(xp), Some(yp), Some(zp)) = (x_period, y_period, z_period) {
-        (a_solution.to_string(), lcm(xp, yp, zp).to_string())
-    } else {
-        unreachable!();
-    }
+    (
+        a_solution.to_string(),
+        lcm(periods[0], periods[1], periods[2]).to_string(),
+    )
 }
