5be6bd0c2e0060376d3deb5b804ce23a21b27aa0
Emil Lundberg <emil@emlun.se>
2020-08-20 23:15:45 +0200

    Move unused code into "pretty" module

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,114 +1,7 @@
 use crate::common::Solution;
 use crate::intcode::IntcodeComputer;
 use crate::util::sign;
-use std::collections::HashMap;
 use std::collections::HashSet;
-
-type Point = (i64, i64);
-
-const ENABLE_OUTPUT: bool = false;
-
-#[derive(Eq, PartialEq)]
-enum Tile {
-    Empty,
-    Wall,
-    Block,
-    Paddle,
-    Ball,
-}
-
-struct State {
-    world: HashMap<Point, Tile>,
-    score: i64,
-    paddle_x: i64,
-    ball_x: i64,
-}
-
-impl State {
-    fn new() -> State {
-        State {
-            world: HashMap::new(),
-            score: 0,
-            paddle_x: 0,
-            ball_x: 0,
-        }
-    }
-}
-
-fn print_state(state: &State) {
-    let minx = *state.world.keys().map(|(x, _)| x).min().unwrap_or(&0);
-    let maxx = *state.world.keys().map(|(x, _)| x).max().unwrap_or(&0);
-    let miny = *state.world.keys().map(|(_, y)| y).min().unwrap_or(&0);
-    let maxy = *state.world.keys().map(|(_, y)| y).max().unwrap_or(&0);
-
-    println!(
-        "\nScore: {}\n{}",
-        state.score,
-        (miny..=maxy)
-            .rev()
-            .rev()
-            .map(|y| {
-                (minx..=maxx)
-                    .map(
-                        |x| match *state.world.get(&(x, y)).unwrap_or(&Tile::Empty) {
-                            Tile::Empty => " ",
-                            Tile::Wall => "#",
-                            Tile::Block => "B",
-                            Tile::Paddle => "-",
-                            Tile::Ball => "o",
-                        },
-                    )
-                    .collect::<Vec<&str>>()
-                    .join("")
-            })
-            .collect::<Vec<String>>()
-            .join("\n")
-    );
-}
-
-#[allow(dead_code)]
-fn play_game(mut computer: IntcodeComputer) -> State {
-    let mut state: State = State::new();
-    computer.run_mut(Some(0));
-
-    while computer.is_running() {
-        let joystick = sign(state.ball_x - state.paddle_x);
-        computer.run_mut(Some(joystick));
-
-        while !computer.output.is_empty() {
-            let (x, y) = (
-                computer.output.pop_front().unwrap(),
-                computer.output.pop_front().unwrap(),
-            );
-
-            if (x, y) == (-1, 0) {
-                state.score = computer.output.pop_front().unwrap();
-            } else {
-                let output_tile_id = match computer.output.pop_front().unwrap() {
-                    0 => Tile::Empty,
-                    1 => Tile::Wall,
-                    2 => Tile::Block,
-                    3 => {
-                        state.paddle_x = x;
-                        Tile::Paddle
-                    }
-                    4 => {
-                        state.ball_x = x;
-                        Tile::Ball
-                    }
-                    _ => unreachable!(),
-                };
-                state.world.insert((x, y), output_tile_id);
-            }
-        }
-
-        if ENABLE_OUTPUT {
-            print_state(&state);
-        }
-    }
-
-    state
-}
 
 fn solve_a(mut computer: IntcodeComputer) -> usize {
     computer.run_mut(None);
@@ -173,4 +66,117 @@ pub fn solve(lines: &[String]) -> Solution {
         solve_a(computer.clone()).to_string(),
         solve_b(computer).to_string(),
     )
+}
+
+mod pretty {
+    use crate::intcode::IntcodeComputer;
+    use crate::util::sign;
+    use std::collections::HashMap;
+
+    type Point = (i64, i64);
+
+    const ENABLE_OUTPUT: bool = false;
+
+    #[derive(Eq, PartialEq)]
+    enum Tile {
+        Empty,
+        Wall,
+        Block,
+        Paddle,
+        Ball,
+    }
+
+    struct State {
+        world: HashMap<Point, Tile>,
+        score: i64,
+        paddle_x: i64,
+        ball_x: i64,
+    }
+
+    impl State {
+        fn new() -> State {
+            State {
+                world: HashMap::new(),
+                score: 0,
+                paddle_x: 0,
+                ball_x: 0,
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    fn print_state(state: &State) {
+        let minx = *state.world.keys().map(|(x, _)| x).min().unwrap_or(&0);
+        let maxx = *state.world.keys().map(|(x, _)| x).max().unwrap_or(&0);
+        let miny = *state.world.keys().map(|(_, y)| y).min().unwrap_or(&0);
+        let maxy = *state.world.keys().map(|(_, y)| y).max().unwrap_or(&0);
+
+        println!(
+            "\nScore: {}\n{}",
+            state.score,
+            (miny..=maxy)
+                .rev()
+                .rev()
+                .map(|y| {
+                    (minx..=maxx)
+                        .map(
+                            |x| match *state.world.get(&(x, y)).unwrap_or(&Tile::Empty) {
+                                Tile::Empty => " ",
+                                Tile::Wall => "#",
+                                Tile::Block => "B",
+                                Tile::Paddle => "-",
+                                Tile::Ball => "o",
+                            },
+                        )
+                        .collect::<Vec<&str>>()
+                        .join("")
+                })
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
+    }
+
+    #[allow(dead_code)]
+    fn play_game(mut computer: IntcodeComputer) -> State {
+        let mut state: State = State::new();
+        computer.run_mut(Some(0));
+
+        while computer.is_running() {
+            let joystick = sign(state.ball_x - state.paddle_x);
+            computer.run_mut(Some(joystick));
+
+            while !computer.output.is_empty() {
+                let (x, y) = (
+                    computer.output.pop_front().unwrap(),
+                    computer.output.pop_front().unwrap(),
+                );
+
+                if (x, y) == (-1, 0) {
+                    state.score = computer.output.pop_front().unwrap();
+                } else {
+                    let output_tile_id = match computer.output.pop_front().unwrap() {
+                        0 => Tile::Empty,
+                        1 => Tile::Wall,
+                        2 => Tile::Block,
+                        3 => {
+                            state.paddle_x = x;
+                            Tile::Paddle
+                        }
+                        4 => {
+                            state.ball_x = x;
+                            Tile::Ball
+                        }
+                        _ => unreachable!(),
+                    };
+                    state.world.insert((x, y), output_tile_id);
+                }
+            }
+
+            if ENABLE_OUTPUT {
+                print_state(&state);
+            }
+        }
+
+        state
+    }
 }
